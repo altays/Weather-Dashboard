15b9a7edba60569325dca568f9ae336ed01d0aaa
Alex Taylor <altays633@gmail.com>
2020-03-02 11:35:58 -0500

    added ajax calls for one day weather, uv. Five day in progress

--- assets/script.js
+++ assets/script.js
@@ -18,31 +18,136 @@
 // #search-history
 
 let appID="d4c97eb69f4c233c45990ad2f9bc1349";
-// let queryURL="api.openweathermap.org/data/2.5/weather"
 
-// use one query url for one day weather - use city input
-// use another query url for uv index - pull lat & long from one day weather
-// use another query url for 5 day forecast - use city input
+// object variables
 
-// check out https://css-tricks.com/multiple-simultaneous-ajax-requests-one-callback-jquery/ for multiple AJAX Calls
+//one day
+
+let selectionLocation = "Columbia,MD,USA";
+let cityName;
+let currentDate;
+let weatherIcon;
+let currentTemp;
+let currentHumid;
+let currentWindSpeed;
+let currentWindDegrees
+let currentLat;
+let currentLon;
+
+//used for exploring object in console
+let responseVal;
+
+// uv index
+
+let uvValue;
+
+//5 day
+
+let dayOneDate;
+let dayOneIcon;
+let dayOneTemp;
+let dayOneHumid;
+
+let dayTwoDate;
+let dayTwoIcon;
+let dayTwoTemp;
+let dayTwoHumid;
+
+let dayThreeDate;
+let dayThreeIcon;
+let dayThreeTemp;
+let dayThreeHumid;
+
+let dayFourDate;
+let dayFourIcon;
+let dayFourTemp;
+let dayFourHumid;
+
+let dayFiveDate;
+let dayFiveIcon;
+let dayFiveTemp;
+let dayFiveHumid;
+
+// helper functions
+
+function kelvinToFahrenheit(temp) {
+    return (temp - 273.15 ) * 1.8;
+}
+
+function calculateAverageFiveDay(data1,data2,data3,data4,data5) {
+    return (data1+data2+data3+data4+data5)/5;
+}
+
+// one day
 
 $.ajax(({
-    url: "http://api.openweathermap.org/data/2.5/weather?q=Columbia,MD,USA&APPID=d4c97eb69f4c233c45990ad2f9bc1349",
+    url: "http://api.openweathermap.org/data/2.5/weather?q=" + selectionLocation + "&APPID=" + appID,
     method: "POST"
 })).then(function(response){
-    console.log("One Day: " + JSON.stringify(response));
+    // console.log("One Day: " + JSON.stringify(response));
+    cityName = response.name;
+    currentDate;
+    weatherIcon = response.weather[0].icon;
+    currentTempKelvin = response.main.temp;
+    currentTempFahrenheit = kelvinToFahrenheit(currentTempKelvin).toFixed(2);
+    currentHumid= response.main.humidity;
+    currentWindSpeed = response.wind.speed;
+    currentWindDegrees = response.wind.deg;
+    currentLat = response.coord.lat;
+    currentLon = response.coord.lon;
+    console.log("City Name: " + cityName + " \n Current Date: " + currentDate + "\nWeather Icon: " + weatherIcon + "\nCurrent Temp: " + currentTempFahrenheit);
+    console.log("Current Humidity: " + currentHumid + "\n Current WindSpeed: " + currentWindSpeed + "\nCurrent Wind Direction: " + currentWindDegrees);
+    console.log("Current Latitude: " + currentLat + "\n Current Long: " + currentLon);
+
+    $.ajax(({
+        url: "http://api.openweathermap.org/data/2.5/uvi?APPID="+appID+"&lat="+currentLat + "&lon=" + currentLon,
+        method: "GET"
+    })).then(function(responseUV){
+        // console.log("UV Index: " + JSON.stringify(responseUV));
+        uvValue=responseUV.value;
+        console.log("Current UV Index: " + uvValue);
+        
+    })
 })
 
-$.ajax(({
-    url: "http://api.openweathermap.org/data/2.5/forecast?q=Columbia,MD,USA&APPID=d4c97eb69f4c233c45990ad2f9bc1349",
-    method: "GET"
-})).then(function(response){
-    console.log("5 Day: " + JSON.stringify(response));
-})
+// five day
 
 $.ajax(({
-    url: "http://api.openweathermap.org/data/2.5/uvi?APPID=d4c97eb69f4c233c45990ad2f9bc1349&lat=37.73&lon=-75.75",
+    url: "http://api.openweathermap.org/data/2.5/forecast?q=" + selectionLocation + "&APPID=" + appID,
     method: "GET"
-})).then(function(response){
-    console.log("UV Index: " + JSON.stringify(response));
+})).then(function(responseFiveDay){
+    // console.log("5 Day: " + JSON.stringify(responseFiveDay));
+    responseVal=responseFiveDay;
+
+
+    dayOneDate=responseFiveDay.list[0].dt_txt;
+    dayOneIcon=responseFiveDay.list[0].weather[0].icon;
+    // pull date from all four three hour groups, average together, convert to Fahrenheit
+    dayOneTemp;
+    // pull data from all four thee hour groups, average together
+    dayOneHumid;
+
+    dayTwoDate;
+    dayTwoIcon;
+    dayTwoTemp;
+    dayTwoHumid;
+    
+    dayThreeDate;
+    dayThreeIcon;
+    dayThreeTemp;
+    dayThreeHumid;
+    
+    dayFourDate;
+    dayFourIcon;
+    dayFourTemp;
+    dayFourHumid;
+    
+    dayFiveDate;
+    dayFiveIcon;
+    dayFiveTemp;
+    dayFiveHumid;
 })
+
+// uv index
+
+
